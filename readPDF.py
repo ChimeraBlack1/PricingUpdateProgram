2de57c17c79c6a38fe9b3b90aebb3aac18405dce
chimerablack1 <christopherparke@zoho.com>
2019-08-12 20:48:06 -0400

    figured out to split the text properly on newlines, I would need a space between ' and the \n like ' \n '

--- readPDF.py
+++ readPDF.py
@@ -17,21 +17,23 @@ pageObj = pdfReader.getPage(0)
 extractedText = pageObj.extractText()
 
 # split text at whitespace
-extractedText = extractedText.split('\n')
+extractedText = extractedText.split(' \n ')
 
 #Item Data
 itemNumber = extractedText[0]
-itemName = extractedText[2]
-itemDesc = extractedText[4]
-itemMAPP = extractedText[6]
+itemName = extractedText[1]
+itemDesc = extractedText[2]
+itemMAPP = extractedText[3]
 itemRMAP1 = extractedText[4]
 itemRMAP2 = extractedText[5]
 
 # print variables
-print("item Number: " + itemNumber)
+# print("item Number: " + itemNumber + " item Name: " + itemName + " item Desc: " + itemDesc + " item MAPP: " + itemMAPP + " item RMAPP1: " + itemRMAP1 + "item RMAPP2: " + itemRMAP2)
 print("item Name: " + itemName)
 print("item Desc: " + itemDesc)
 print("item MAPP: " + itemMAPP)
+print("item RMAPP1: " + itemRMAP1)
+print("item RMAPP2: " + itemRMAP2)
 print(extractedText)
 # closing the pdf file object 
 pdfFileObj.close()
